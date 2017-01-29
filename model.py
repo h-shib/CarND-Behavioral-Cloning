a34176447806371cc88bde88e67f1e4bf37cbbbe
h-shib <hiraku.shibuya888@gmail.com>
2017-01-29 22:42:09 +0900

    training with left and right images

--- model.py
+++ model.py
@@ -13,16 +13,16 @@ from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
 
-image_file_path = '/Users/hiraku/Desktop/IMG/'
-driving_file_path = '/Users/hiraku/Desktop/driving_log.csv'
+image_file_path = '/Users/hiraku/Desktop/driving_data/IMG/'
+driving_file_path = '/Users/hiraku/Desktop/driving_data/driving_log.csv'
 images = os.listdir(image_file_path)
 
 def load_data():
-	train_imgs = []
-	train_imgs = np.array([mpimg.imread(image_file_path + img) for img in images])
-	print(train_imgs)
-
-	train_labels = pd.read_csv(driving_file_path, header=None).iloc[:, 3]
+	train_imgs = [mpimg.imread(image_file_path + img) for img in images]
+	train_imgs = np.array(train_imgs)
+	print("finish train_imgs")
+	train_labels = pd.read_csv(driving_file_path).iloc[:, 3]
+	train_labels = np.concatenate((train_labels, (train_labels+.2), (train_labels-.2)), axis=0)
 
 	return train_test_split(train_imgs, train_labels, test_size=0.3, random_state=0)
 
@@ -35,12 +35,8 @@ def main():
 	input_shape = X_train.shape[1:]
 	output_shape = len(np.unique(y_train))
 
-	print(input_shape)
-	print(output_shape)
-
 	model = Sequential()
-	model.add(Convolution2D(32, 3, 3, border_mode='same',
-                        input_shape=(160,320,3,)))
+	model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(160,320,3,)))
 	model.add(Activation('relu'))
 	model.add(Convolution2D(32, 3, 3))
 	model.add(Activation('relu'))
@@ -67,15 +63,11 @@ def main():
 	#model = Model(input=inputs, output=prediction)
 	model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-	model.fit(X_train, y_train, shuffle=True, validation_data=(X_test, y_test), nb_epoch=5)
+	model.fit(X_train, y_train, shuffle=True, validation_data=(X_test, y_test), nb_epoch=1)
 
-	print(model.get_config)
 	with open('model.json', 'w') as f:
 		f.write(model.to_json())
 	model.save_weights('model.h5')
 
 if __name__ == '__main__':
 	main()
-	
-    
-
