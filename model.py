c7f4ba8525f62a00e0facec11491ccc8291427be
h-shib <hiraku.shibuya888@gmail.com>
2017-02-07 07:14:06 +0900

    implement model

--- model.py
+++ model.py
@@ -1,99 +1,91 @@
 import os
-import pickle
-import tensorflow as tf
+import cv2
+import csv
+import math
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from keras.layers import Input, Flatten, Dense
-from keras.models import Model, Sequential
-from keras.optimizers import Adam
-from sklearn.model_selection import train_test_split
-from keras.layers.core import Dense, Dropout, Activation, Flatten, Lambda
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.advanced_activations import ELU
-from keras.layers.normalization import BatchNormalization
 from scipy.misc import imresize
+import matplotlib.image as mpimg
+from keras.models import Model, Sequential
+from sklearn.model_selection import train_test_split
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
 
 image_file_path = './data/IMG/'
 driving_file_path = './data/driving_log.csv'
-images = os.listdir(image_file_path)
+
+conv_layers = [32, 32, 64, 128]
+dense_layers = [1024, 512, 256]
+
+def model(input_shape):
+	model = Sequential()
+	model.add(Convolution2D(32, 3, 3, activation='elu', input_shape=input_shape))
+	model.add(MaxPooling2D())
+	for layers in conv_layers:
+		model.add(Convolution2D(layers, 3, 3, activation='elu'))
+		model.add(MaxPooling2D())
+	model.add(Flatten())
+	for layers in dense_layers:
+		model.add(Dense(layers, activation='elu'))
+		model.add(Dropout(0.5))
+	model.add(Dense(1, activation='linear'))
+	model.compile(loss='mse', optimizer='adam')
+	return model
 
 def load_data():
-	train_imgs = [imresize(mpimg.imread(image_file_path + img)[60:130], (66, 200, 3)) for img in images]
-	train_imgs = np.array(train_imgs)
-	print("finish train_imgs")
-	train_labels = pd.read_csv(driving_file_path).iloc[:, 3]
-	train_labels = np.concatenate((train_labels, (train_labels+.2), (train_labels-.2)), axis=0)
+	X, y = [], []
+	offset = 0.4
+	with open(driving_file_path) as f:
+		reader = csv.reader(f)
+		next(reader)
+		for center, left, right, steering, _, _, _ in reader:
+			X += [left.strip(), right.strip()]
+			y += [float(steering)+offset, float(steering)-offset]
+	return X, y
 
-	return train_test_split(train_imgs, train_labels, test_size=0.3, random_state=0)
+def preprocess_image(image_path, steering, shape=(100, 100)):
+	image = mpimg.imread(image_path)
+	origin_shape = image.shape
+	image = image[math.floor(float(origin_shape[0])/5):origin_shape[0]-25, 0:origin_shape[1]]
+	image = imresize(mpimg.imread(image_path), shape)
+	image = random_brightness(image)
+	image, steering = random_flip(image, steering)
+	image = (image / 255. - 0.5)
+	return image, steering
+
+def random_flip(image, steering):
+	is_flip = np.random.randint(2)
+	if is_flip:
+		image = cv2.flip(image, 1)
+		steering = -steering
+	return image, steering
+
+def random_brightness(image):
+	image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+	random_brightness = np.random.uniform() + 0.25
+	image[:, :, 2] = image[:, :, 2] * random_brightness
+	image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+	return image
+
+def _generator(batch_size, X, y):
+	while 1:
+		batch_X, batch_y = [], []
+		for i in range(batch_size):
+			random_idx = np.random.randint(len(X))
+			steering = y[random_idx]
+			image_path = "./data/" + X[random_idx]
+			image, steering = preprocess_image(image_path, steering)
+			batch_X.append(image)
+			batch_y.append(steering)
+		yield np.array(batch_X), np.array(batch_y)
 
 def main():
-	X_train, X_test, y_train, y_test = load_data()
-
-	print("Shape of Train: ", X_train.shape, y_train.shape)
-	print("Shape of Test: ", X_test.shape, y_test.shape)
-
-	input_shape = X_train.shape[1:]
-	output_shape = len(np.unique(y_train))
-
-	model = Sequential()
-	# Input Layer.
-	# Input: 66x200x3. Output: normalized input
-	model.add(Lambda(lambda x : x/127.5 - 1., input_shape = (66, 200, 3)))
-	# Layer 1: Convolutional Layer.
-	# Input: 66x200x3. Output: 66x200x24.
-	model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode='valid', init='he_normal'))
-	model.add(ELU())
-	# Layer 2: Convolutional Layer.
-	# Input: 66x200x24. Output: 66x200x36.
-	model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode='valid', init='he_normal'))
-	model.add(ELU())
-	# Layer 3: Convolutional Layer.
-	# Input: 66x200x36. Output: 66x200x48.
-	model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode='valid', init='he_normal'))
-	model.add(ELU())
-	# Layer 4: Convolutional Layer.
-	# Input: 66x200x48. Output: 66x200x64.
-	model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='valid', init='he_normal'))
-	model.add(ELU())
-	# Layer 5: Convolutional Layer.
-	# Input: 66x200x64. Ouput: 66x200x64.
-	model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='valid', init='he_normal'))
-	model.add(ELU())
-	model.add(Flatten())
-	# Layer 6: Fully Connected Layer.
-	# Input: 844800. Output: 1164.
-	model.add(Dense(1164, init='he_normal'))
-	model.add(ELU())
-	# Layer 7: Fully Connected Layer.
-	# Input: 1164. Output: 100.
-	model.add(Dense(100, init='he_normal'))
-	model.add(ELU())
-	# Layer 8: Fully Connected Layer.
-	# Input: 100. Output: 100.
-	model.add(Dense(100, init='he_normal'))
-	model.add(ELU())
-	# Layer 9: Fully Connected Layer. (with dropout rate of 0.4)
-	# Input: 100. Output: 10.
-	model.add(Dropout(0.4))
-	model.add(Dense(10, init='he_normal'))
-	model.add(ELU())
-	# Output Layer.
-	model.add(Dense(1, init='he_normal'))
-
-	#inputs = Input(shape=input_shape)
-	#x = Flatten()(inputs)
-	#prediction = Dense(output_shape, activation='softmax')(x)
-
-	#model = Model(input=inputs, output=prediction)
-	model.compile(loss='mean_squared_error', optimizer='adam')
-
-	model.fit(X_train, y_train, shuffle=True, validation_data=(X_test, y_test), nb_epoch=1)
-
-	with open('model.json', 'w') as f:
-		f.write(model.to_json())
-	model.save_weights('model.h5')
+	X, y = load_data()
+	net = model(input_shape=(100, 100, 3))
+	net.fit_generator(_generator(256, X, y), samples_per_epoch=20224, nb_epoch=8)
+	with open('checkpoints/model.json', 'w') as f:
+		f.write(net.to_json())
+	net.save_weights('./checkpoints/model.h5')
+	print("model saved")
 
 if __name__ == '__main__':
 	main()
